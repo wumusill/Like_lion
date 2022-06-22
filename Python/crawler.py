2873e0ed5814b4f67b9a0b560392def628f5f705
JaHyeon_Gu <jahyungu@naver.com>
2022-06-22 23:44:40 +0900

    contents of the lecture about BeautifulSoup on Jun 21th

--- Python/crawler.py
+++ Python/crawler.py
@@ -1,3 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
-print(requests) 
+url = "http://wwww.daum.net"
+response = requests.get(url)
+
+# print(response)     # Response [200]
+# print(response.text)      # html
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.title)
